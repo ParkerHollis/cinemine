22bfdd55cb50f90424f6cb9d99c39d891c77649c
Parker Hollis <me@vzg.me>
2014-10-08 20:44:12 -0500

    Re-added CPU information to probe.
    Hopefully it won't mess up this time.

--- probe.js
+++ probe.js
@@ -2,18 +2,24 @@ var application_root = __dirname,
 express = require( 'express' );
 var app = express();
 var os = require('os');
+var utils  = require('os-utils');
 
 app.get( '/stats', function( request, response ) {
 	var uptime = Math.round(os.uptime());
 	var totalmem = Math.round(os.totalmem());
 	var freemem = Math.round(os.freemem());
 	var hostname = os.hostname();
-	var stats = {
+	utils.cpuUsage(function(v){
+		var cpu = Math.floor(v*1000)/10;
+		var stats = {
                 uptime: uptime,
                 freemem: freemem,
-				totalmem: totalmem
+				totalmem: totalmem,
+				cpu: cpu
             };
-    response.send(stats);
+		response.send(stats);
+		console.log(cpu);
+	});
 });
 var port = 4711;
 app.listen( port, function() {
